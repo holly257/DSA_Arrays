14f0dbd81a875e952d86c0836b684db920dcebc9
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-09 13:56:44 -0400

    finished 5, 6, and 9

--- assignment.js
+++ assignment.js
@@ -14,7 +14,6 @@ function noSpace(value) {
 //console.log(noSpace('www.thinkful.com /tauh ida parv een'));
 
 //6. Filtering an array
-////    in progress
 function lessThanFive(numArray) {
     let lessThan = [];
     let item = null;
@@ -25,51 +24,55 @@ function lessThanFive(numArray) {
     if (numArray[0] < 5) {
         let result = lessThanFive(numArray.slice(1));
         item = numArray[0];
-    
+        lessThan.push(item)
+
         if (result.length != 0) {
             result.forEach(num => lessThan.push(num));
         }
     } else lessThanFive(numArray.slice(1));
 
-    lessThan.push(item);
     return lessThan;
 }
 
-console.log(lessThanFive([1, 2, 3, 4, 5, 6, 1, 7, 8]));
+//console.log(lessThanFive([1, 2, 3, 4, 5, 6, 1, 7, 8]));
 
-//7. 
-//not what they were asking for 
+//7. Max sum in the array - for sequence of contiguous nums
+//not what they were asking for
 function sum(numArr) {
-    let pos = 0;
-    let neg = 0;
+    //keep track of highest sum
+    let highestSum = 0;
+    
+    //generate sequences 
 
-    numArr.forEach(num => {
-        if (num > 0){
-            pos = pos + num
-            return pos;
-        } else {
-            neg = neg + num
-            return neg;
-        }
-    })
-
-    let sum = pos + neg
-    return sum 
+    //need start loop
+    //need end loop
+    //third to loop through
+    //- find sum of each
+    
 }
 
-console.log(sum([4, 6, -3, 5, -2, 1]))
+//console.log(sum([4, 6, -3, 5, -2, 1]))
 
-//8.
-//9.
+//8. Merge arrays
+
+//9. Remove characters
 function removeChar(value, char) {
-    if (value === '') {
+    if (!value.length || !char.length) {
         return '';
     }
-    //in progress
-    if (value[0] == ' ') {
-        return '%20' + noSpace(value.slice(1));
+
+    if (char.includes(value[0])) {
+        return '' + removeChar(value.slice(1), char);
     }
-    return value[0] + noSpace(value.slice(1));
+
+    return value[0] + removeChar(value.slice(1), char);
 }
 
-console.log(removeChar('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'))
+//console.log(removeChar('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'));
+//Output: 'Bttl f th Vwls: Hw vs. Grzny'
+
+// 10. Products
+// 11. 2D array
+// 12. String rotation
+
+//assign Big O for all functions
